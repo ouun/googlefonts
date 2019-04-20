2cd493ce6e6952e5dc3398df18b3fc24349fa706
Ari <aristath@gmail.com>
2019-04-20 15:40:37 +0300

    Add get_google_fonts method

--- src/GoogleFonts.php
+++ src/GoogleFonts.php
@@ -18,6 +18,16 @@ namespace Kirki;
 final class GoogleFonts {
 
 	/**
+	 * An array of our google fonts.
+	 *
+	 * @static
+	 * @access public
+	 * @since 0.1
+	 * @var array
+	 */
+	public static $google_fonts;
+
+	/**
 	 * The class constructor.
 	 *
 	 * @access public
@@ -41,5 +51,57 @@ final class GoogleFonts {
 		if ( function_exists( 'wp_die' ) ) {
 			wp_die();
 		}
+	}
+
+	/**
+	 * Return an array of all available Google Fonts.
+	 *
+	 * @access public
+	 * @since 0.1
+	 * @return array All Google Fonts.
+	 */
+	public function get_google_fonts() {
+
+		// Get fonts from cache.
+		if ( function_exists( 'get_site_transient' ) ) {
+			self::$google_fonts = get_site_transient( 'kirki_googlefonts_cache' );
+		}
+
+		// If cache is populated, return cached fonts array.
+		if ( self::$google_fonts ) {
+			return self::$google_fonts;
+		}
+
+		// If we got this far, cache was empty so we need to get from JSON.
+		ob_start();
+		include 'webfonts.json'; // phpcs:ignore WPThemeReview.CoreFunctionality.FileInclude
+
+		$fonts_json = ob_get_clean();
+		$fonts      = json_decode( $fonts_json, true );
+
+		self::$google_fonts = [];
+		if ( is_array( $fonts ) ) {
+			foreach ( $fonts['items'] as $font ) {
+				self::$google_fonts[ $font['family'] ] = [
+					'label'    => $font['family'],
+					'variants' => $font['variants'],
+					'category' => $font['category'],
+				];
+			}
+		}
+
+		$cache_time = 360;
+		// Apply the 'kirki_fonts_google_fonts' filter.
+		if ( function_exists( 'apply_filters' ) ) {
+			self::$google_fonts = apply_filters( 'kirki_fonts_google_fonts', self::$google_fonts );
+			
+			// Save the array in cache.
+			$cache_time = apply_filters( 'kirki_googlefonts_transient_time', $cache_time );
+		}
+		if ( function_exists( 'set_site_transient' ) ) {
+			set_site_transient( 'kirki_googlefonts_cache', self::$google_fonts, $cache_time );
+		}
+
+		return self::$google_fonts;
 	}
 }
